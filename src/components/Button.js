2e2dab767e2a6d238b77245525d4363f463027ff
alvinoalvin <alvinoalvin@gmail.com>
2021-05-10 15:27:02 -0700

    building button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,27 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+   
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  if (props.disabled) {
+    buttonClass += " button--disabled ";
+  }
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
