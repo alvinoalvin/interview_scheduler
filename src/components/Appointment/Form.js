a666e04b833c9609937843d124eb67c5d2176437
alvinoalvin <alvinoalvin@gmail.com>
2021-05-11 22:33:19 -0700

    Form.js changes

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { useState } from 'react'
 import "components/Appointment/styles.scss";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
-  let { name, interviewers, interviewer, setInterviewer, onSave, onCancel } = props;
+  let { interviewers, onSave, onCancel } = props;
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  function reset() {
+    setName("");
+    setInterviewer(null);
+  }
+
+  function cancel() {
+    reset();
+    onCancel();
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name={name}
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-          */
+            onChange={event => setName(event.target.value)}
           />
         </form>
-        <InterviewerList interviewers={interviewers} value={interviewer} onChange={setInterviewer} />
+        <InterviewerList interviewers={interviewers} value={interviewer} onChange={event => setInterviewer(event)} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
-          <Button confirm onClick={onSave} >Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onSubmit={event => event.preventDefault()} onClick={onSave} >Save</Button>
         </section>
       </section>
-    </main>
+    </main >
 
   );
 }
